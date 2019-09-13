d10af7136c08665d88ee7793af23b9db4299a380
Luke Hayes <lukehayes2021@u.northwestern.edu>
2019-09-13 15:19:13 -0500

    Server Set Up

---
+++ server.js
@@ -0,0 +1,24 @@
+// Required dependencies
+var express = require('express');
+var path = require('path');
+
+// Configure the Express application
+var app = express();
+var PORT = process.env.PORT || 3030;
+
+// Expose the public directory to access CSS files
+app.use(express.static('public'));
+app.use(express.static('public/css'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Add the application routes
+require(path.join(__dirname, './routes/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('---------------------------------------');
+  console.log('Portfolio is listening on PORT: ' + PORT);
+  console.log('----------------------------------------');
+});
