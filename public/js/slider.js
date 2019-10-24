c9ac104a52352bbba1a4f88601add9f3c9a6aab9
Luke Hayes <lukehayes2021@u.northwestern.edu>
2019-10-24 18:01:42 -0500

    update

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,58 +1,74 @@
-( function($) {
+// ( function($) {
   
-    $(document).ready(function() {
+//     $(document).ready(function() {
       
-      var s           = $('.slider'),
-          sWrapper    = s.find('.slider-wrapper'),
-          sItem       = s.find('.slide'),
-          btn         = s.find('.slider-link'),
-          sWidth      = sItem.width(),
-          sCount      = sItem.length,
-          slide_date  = s.find('.slide-date'),
-          slide_title = s.find('.slide-title'),
-          slide_text  = s.find('.slide-text'),
-          slide_more  = s.find('.slide-more'),
-          slide_image = s.find('.slide-image img'),
-          sTotalWidth = sCount * sWidth;
+//       var s           = $('.slider'),
+//           sWrapper    = s.find('.slider-wrapper'),
+//           sItem       = s.find('.slide'),
+//           btn         = s.find('.slider-link'),
+//           sWidth      = sItem.width(),
+//           sCount      = sItem.length,
+//           slide_date  = s.find('.slide-date'),
+//           slide_title = s.find('.slide-title'),
+//           slide_text  = s.find('.slide-text'),
+//           slide_more  = s.find('.slide-more'),
+//           slide_image = s.find('.slide-image img'),
+//           sTotalWidth = sCount * sWidth;
       
-      sWrapper.css('width', sTotalWidth);
-      sWrapper.css('width', sTotalWidth);
+//       sWrapper.css('width', sTotalWidth);
+//       sWrapper.css('width', sTotalWidth);
       
-      var clickCount  = 0;
+//       var clickCount  = 0;
       
-      var nextSlide = function(e,dir) {
-        if(e!=="custom"){
-          e.preventDefault();
-        }
+//       var nextSlide = function(e,dir) {
+//         if(e!=="custom"){
+//           e.preventDefault();
+//         }
   
-        if( $(this).hasClass('next') || dir === "next") {
+//         if( $(this).hasClass('next') || dir === "next") {
           
-          ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
-        } else if ( $(this).hasClass('prev') ) {
+//           ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
+//         } else if ( $(this).hasClass('prev') ) {
           
-          ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
-        }
-        TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
+//           ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
+//         }
+//         TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
   
   
-        //CONTENT ANIMATIONS
+//         //CONTENT ANIMATIONS
   
-        var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
-        var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
+//         var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
+//         var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
   
-        TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
-        TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
-        TweenLite.fromTo(slide_title, 0.6, fromProperties, toProperties);
-        TweenLite.fromTo(slide_text, 0.8, fromProperties, toProperties);
-        TweenLite.fromTo(slide_more, 1, fromProperties, toProperties);
+//         TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
+//         TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
+//         TweenLite.fromTo(slide_title, 0.6, fromProperties, toProperties);
+//         TweenLite.fromTo(slide_text, 0.8, fromProperties, toProperties);
+//         TweenLite.fromTo(slide_more, 1, fromProperties, toProperties);
   
-      }
+//       }
   
-      btn.on('click', nextSlide);
-      setInterval(()=>{
-        nextSlide("custom","next");
-      },30000)
-    });
-  })(jQuery);
+//       btn.on('click', nextSlide);
+//       setInterval(()=>{
+//         nextSlide("custom","next");
+//       },30000)
+//     });
+//   })(jQuery);
   
-  $('.overlay').addClass('overlay-blue');
+//   $('.overlay').addClass('overlay-blue');
+
+$(function() {
+ 
+	$('#carousel').carouFredSel({
+		responsive: true,
+		scroll: 1,
+		items: {
+			width: 300,
+			visible: {
+				min: 3,
+				max: 9
+			}
+		}
+	});
+ 
+});
